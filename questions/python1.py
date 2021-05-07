88c25f18643f47c938a590b563fdad36aa8695a1
Your Name <you@example.com>
2021-05-07 10:28:50 +0100

    Question 2

--- questions/python1.py
+++ questions/python1.py
@@ -64,6 +64,12 @@ def one(input1, input2):
 	# No hints for this question
 
 def two(arg1):
+	if arg1 % 3 == 0 and arg1 % 5 == 0:
+		return "fizzbuzz"
+	elif arg1 % 5 == 0:
+		return "buzz"
+	elif arg1 % 3 == 0:
+		return "fizz"
 	return "null"
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
